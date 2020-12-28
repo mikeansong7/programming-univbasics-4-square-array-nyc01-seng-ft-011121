229875efcfec8d8a2193a993bd9e77db24d4aca8
Michael B Ansong <michael.ansong7@gmail.com>
2020-12-28 17:54:28 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,18 @@
-def square_array(array)
-  # your code here
+#def square_array(numbers)
+  #new_numbers = []
+  #numbers.each do {|num|new_numbers << num**2}
+  #end
+  #new_numbers
+#end
+
+def square_array(numbers)
+  new_numbers = []
+  i=0
+  while i < numbers.length do
+    new_numbers << numbers[i]**2 
+    i += 1
+  end
+  new_numbers
 end
+    
+
