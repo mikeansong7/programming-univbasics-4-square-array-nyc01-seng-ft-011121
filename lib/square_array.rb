98fd0190631fea1c1be7bcc1b8acba5b7b804b7f
Michael B Ansong <michael.ansong7@gmail.com>
2020-12-28 17:52:29 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -9,7 +9,10 @@ def square_array(numbers)
   new_numbers = []
   i=0
   while i < numbers.length do
-    new_numbers << numbers**2 
+    new_numbers << numbers[i]**2 
+    i += 1
   end
+  new_numbers
+end
     
 
